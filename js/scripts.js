ea35417380251947b016ffcb8bf54d3ee0419791
Steven <slangford@engr-134-10.eastern.ewu.edu>
2014-11-24 14:39:22 -0800

    Version 2

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $.simpleWeather({
+    
+    location: 'Spokane, WA',
+    woeid: '',
+    unit: 'f',
+    
+    // if can get weather
+    success: function(weather) {
+      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      html += '<li class="currently">'+weather.currently+'</li>';
+      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+      
+      // PUT DISPLAY
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
+});
